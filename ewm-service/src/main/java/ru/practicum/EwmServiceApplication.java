71986e6355d007468c18cc29497c4d2fc1d31a8b
MCherniavski <mariechernyavskaya@gmail.com>
2023-08-14 23:29:41 +0300

    first review

--- ewm-service/src/main/java/ru/practicum/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/EwmServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServiceApplication {
+public class EwmServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApplication.class, args);
+        SpringApplication.run(EwmServiceApplication.class, args);
     }
 }
